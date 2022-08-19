2a2a32f3840ea583b8f325dfef3a90c5c5a4163e
Tyler Hawkins <thawkin3@byu.net>
2022-08-19 13:18:14 -0600

    fix: returns result faster in recursive solution

--- find-key-in-boxes/find-key-in-boxes-loop.js
+++ find-key-in-boxes/find-key-in-boxes-loop.js
@@ -1,64 +1,4 @@
-// Box A has three boxes inside it: Box B, Box C, and Box D
-// Box B has nothing inside it
-// Box C has two boxes inside it: Box E and Box F
-// Box E has one box inside it: Box G
-// Box G has the key inside it
-// Box F has nothing inside it
-// Box D one box inside it: Box H
-// Box H has nothing inside it
-
-const box = {
-  type: 'box',
-  id: 'A',
-  contents: [
-    {
-      type: 'box',
-      id: 'B',
-      contents: [],
-    },
-    {
-      type: 'box',
-      id: 'C',
-      contents: [
-        {
-          type: 'box',
-          id: 'E',
-          contents: [
-            {
-              type: 'box',
-              id: 'G',
-              contents: [
-                {
-                  type: 'key',
-                  id: 'Key',
-                  contents: [],
-                },
-              ],
-            },
-          ],
-        },
-        {
-          type: 'box',
-          id: 'F',
-          contents: [],
-        },
-      ],
-    },
-    {
-      type: 'box',
-      id: 'D',
-      contents: [
-        {
-          type: 'box',
-          id: 'H',
-          contents: [],
-        },
-      ],
-    },
-  ],
-};
-
-const findKeyInBox = (box) => {
+export const findKeyInBox = (box) => {
   const pile = [];
   pile.push(box);
 
@@ -68,7 +8,8 @@ const findKeyInBox = (box) => {
 
     for (let i = 0; i < currentBox.contents.length; i++) {
       if (currentBox.contents[i].type === 'key') {
-        return console.log(`Found the key in Box ${currentBox.id}!`);
+        console.log(`Found the key in Box ${currentBox.id}!`);
+        return currentBox.id;
       }
 
       if (currentBox.contents[i].type === 'box') {
---
+++ find-key-in-boxes/find-key-in-boxes-loop.test.js
@@ -0,0 +1,93 @@
+import { findKeyInBox } from './find-key-in-boxes-loop';
+
+describe('findKeyInBox', () => {
+  it('finds the key in a set of nested boxes', () => {
+    // Box A has three boxes inside it: Box B, Box C, and Box D
+    // Box B has nothing inside it
+    // Box C has two boxes inside it: Box E and Box F
+    // Box E has one box inside it: Box G
+    // Box G has the key inside it
+    // Box F has nothing inside it
+    // Box D one box inside it: Box H
+    // Box H has nothing inside it
+
+    const box = {
+      type: 'box',
+      id: 'A',
+      contents: [
+        {
+          type: 'box',
+          id: 'B',
+          contents: [],
+        },
+        {
+          type: 'box',
+          id: 'C',
+          contents: [
+            {
+              type: 'box',
+              id: 'E',
+              contents: [
+                {
+                  type: 'box',
+                  id: 'G',
+                  contents: [
+                    {
+                      type: 'key',
+                      id: 'Key',
+                      contents: [],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'box',
+              id: 'F',
+              contents: [],
+            },
+          ],
+        },
+        {
+          type: 'box',
+          id: 'D',
+          contents: [
+            {
+              type: 'box',
+              id: 'H',
+              contents: [],
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(findKeyInBox(box)).toBe('G');
+  });
+
+  it('finds the key in a single box', () => {
+    const box = {
+      type: 'box',
+      id: 'A',
+      contents: [
+        {
+          type: 'key',
+          id: 'Key',
+          contents: [],
+        },
+      ],
+    };
+
+    expect(findKeyInBox(box)).toBe('A');
+  });
+
+  it('returns undefined if no key exists', () => {
+    const box = {
+      type: 'box',
+      id: 'A',
+      contents: [],
+    };
+
+    expect(findKeyInBox(box)).toBe(undefined);
+  });
+});
